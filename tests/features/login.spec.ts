25d1eed41d3aa4cdfa2247df54f4e7e7fede5f45
nzainuddin <nadzainuddin@gmail.com>
2026-01-03 23:54:53 +0800

    update login tests to use fixtures

--- tests/features/login.spec.ts
+++ tests/features/login.spec.ts
@@ -1,32 +1,27 @@
-import { test } from '@playwright/test';
-import { BasePage } from '../../src/pages/base.page';
+import { test } from '../../src/fixtures';
 
-test('Submit empty login form', async ({ page }) => {
-  const basePage = new BasePage(page);
-  await page.goto('/');
-  await basePage.login('', '');
-  await basePage.acceptDialogBox('Please fill out Username and Password.');
+test('Submit empty login form', async ({ accessAsGuest, pages }) => {
+  accessAsGuest;
+  await pages.basePage.login('', '');
+  await pages.basePage.acceptDialogBox('Please fill out Username and Password.');
 });
 
-test('Login with incorrect username', async ({ page }) => {
-  const basePage = new BasePage(page);
-  await page.goto('/');
-  await basePage.login('testuser1', 'password124');
-  await basePage.acceptDialogBox('User does not exist.');
+test('Login with incorrect username', async ({ accessAsGuest, pages }) => {
+  accessAsGuest;
+  await pages.basePage.login('testuser1', 'password124');
+  await pages.basePage.acceptDialogBox('User does not exist.');
 });
 
-test('Login with incorrect password', async ({ page }) => {
-  const basePage = new BasePage(page);
-  await page.goto('/');
-  await basePage.login('testuser', 'password124');
-  await basePage.acceptDialogBox('Wrong password.');
+test('Login with incorrect password', async ({ accessAsGuest, pages }) => {
+  accessAsGuest;
+  await pages.basePage.login('testuser', 'password124');
+  await pages.basePage.acceptDialogBox('Wrong password.');
 });
 
-test('Login with correct username and password', async ({ page }) => {
-  const basePage = new BasePage(page);
-  await page.goto('/');
-  await basePage.login('Limah', 'Limah');
-  await basePage.loggedInUser('Limah');
-  await basePage.logOut();
+test('Login with correct username and password', async ({ accessAsGuest, pages }) => {
+  accessAsGuest;
+  await pages.basePage.login('Limah', 'Limah');
+  await pages.basePage.loggedInUser('Limah');
+  await pages.basePage.logOut();
 });
 
